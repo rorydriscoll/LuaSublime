2922d09206b8513d76aa62dd582f8cf3be35e5c1
Rory Driscoll <rorydriscoll@gmail.com>
2012-11-13 08:30:02 -0800

    Add an option to use the live parser or not

--- ParseLua.py
+++ ParseLua.py
@@ -3,21 +3,25 @@ import sublime_plugin
 import re
 from subprocess import Popen, PIPE
 
+s = sublime.load_settings("Lua Dev.sublime-settings")
+
 class ParseLuaCommand(sublime_plugin.EventListener):
-	
+
 	TIMEOUT_MS = 200
 
 	def __init__(self):
 		self.pending = 0
 
 	def on_modified(self, view):
+		if not s.get("live_parser"):
+			return
 		filename = view.file_name()
 		if not filename or not filename.endswith('.lua'):
 			return
 		self.pending = self.pending + 1
 		sublime.set_timeout(lambda: self.parse(view), self.TIMEOUT_MS)
 
-	def parse(self, view):		
+	def parse(self, view):
 		# Don't bother parsing if there's another parse command pending
 		self.pending = self.pending - 1
 		if self.pending > 0:
@@ -31,7 +35,7 @@ class ParseLuaCommand(sublime_plugin.EventListener):
 		view.erase_regions('lua')
 		# Nothing to do if it parsed successfully
 		if result == 0:
-			return			
+			return
 		# Add regions and place the error message in the status bar
 		sublime.status_message(errors)
 		pattern = re.compile(r':([0-9]+):')
